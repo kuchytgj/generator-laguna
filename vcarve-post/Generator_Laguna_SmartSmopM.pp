21b8f968dddcabe10f0c1267bb21e48c468489ed
Greg Kuchyt <gkuchyt@empiredynamics.us>
2021-01-23 10:57:05 -0500

    Initial commit of VCarve post for SmartShopM

---
+++ vcarve-post/Generator_Laguna_SmartSmopM.pp
@@ -0,0 +1,158 @@
++================================================
++                                                
++ Laguna DSP Handheld Control - Vectric machine output configuration file   
++                                                
++================================================
++                                                
++ History                                        
++                                                
++ Who      When       What                         
++ ======== ========== ===========================
++ Tony     15/11/2005 Written for metric
++ Mark 	   07/11/2013 Modified for Cut3D
++ GregK    01/23/2021 Modified for Generator
++================================================
+
+POST_NAME = "Laguna SmartShopM ATC - metric"
+
+FILE_EXTENSION = "prg"
+
+UNITS = "MM"
+
++------------------------------------------------
++    Line terminating characters                 
++------------------------------------------------
+
+LINE_ENDING = "[13][10]"
+
++------------------------------------------------
++    Block numbering                             
++------------------------------------------------
+
+LINE_NUMBER_START     = 0
+LINE_NUMBER_INCREMENT = 10
+LINE_NUMBER_MAXIMUM = 999999
+
++================================================
++                                                
++    Formating for variables                     
++                                                
++================================================
+
+VAR LINE_NUMBER = [N|A|N|1.0]
+VAR SPINDLE_SPEED = [S|A|S|1.0]
+VAR FEED_RATE = [F|C|F|1.1]
+VAR X_POSITION = [X|C|X|1.3]
+VAR Y_POSITION = [Y|C|Y|1.3]
+VAR Z_POSITION = [Z|C|Z|1.3]
+VAR ARC_CENTRE_I_INC_POSITION = [I|A|I|1.3]
+VAR ARC_CENTRE_J_INC_POSITION = [J|A|J|1.3]
+VAR X_HOME_POSITION = [XH|A|X|1.3]
+VAR Y_HOME_POSITION = [YH|A|Y|1.3]
+VAR Z_HOME_POSITION = [ZH|A|Z|1.3]
+VAR SAFE_Z_HEIGHT = [SAFEZ|A|Z|1.3]
++================================================
++                                                
++    Block definitions for toolpath output       
++                                                
++================================================
+
++---------------------------------------------------
++  Commands output at the start of the file
++---------------------------------------------------
+
+begin HEADER
+"(Filename:  [TP_FILENAME])"
+"[N] G17 G40 G90"
+"[N] G21"
+"[N] M03 [S]"
++"[N] [XH] [YH] [F]"
+
+
++---------------------------------------------------
++  Commands output for rapid moves 
++---------------------------------------------------
+
+begin RAPID_MOVE
+
+"[N] G00 [X] [Y] [Z]"
+
+
++---------------------------------------------------
++  Commands output for the first feed rate move
++---------------------------------------------------
+
+begin FIRST_FEED_MOVE
+
+"[N] G1 [X] [Y] [Z] [F]"
+
+
++---------------------------------------------------
++  Commands output for feed rate moves
++---------------------------------------------------
+
+begin FEED_MOVE
+
+"[N] G1 [X] [Y] [Z]"
+
++---------------------------------------------------
++  Commands output for the first clockwise arc move
++---------------------------------------------------
+
+begin FIRST_CW_ARC_MOVE
+
+"[N] G2 [X] [Y] [I] [J] [F]"
+
++---------------------------------------------------
++  Commands output for clockwise arc  move
++---------------------------------------------------
+
+begin CW_ARC_MOVE
+
+"[N] G2 [X] [Y] [I] [J]"
+
++---------------------------------------------------
++  Commands output for the first counterclockwise arc move
++---------------------------------------------------
+
+begin FIRST_CCW_ARC_MOVE
+
+"[N] G3 [X] [Y] [I] [J] [F]"
+
++---------------------------------------------------
++  Commands output for counterclockwise arc  move
++---------------------------------------------------
+
+begin CCW_ARC_MOVE
+
+"[N] G3 [X] [Y] [I] [J]"
+
++---------------------------------------------------
++  Commands output for a new segment - toolpath
++  with same toolnumber but maybe different feedrates
++---------------------------------------------------
+
+begin NEW_SEGMENT
+
+"[N] M03 [S]"
+
++---------------------------------------------------
++  Commands output for ATC functionality
++---------------------------------------------------
+
+begin TOOLCHANGE
+
+"[N] M5"
+"[N] M6 [T]"
+
++---------------------------------------------------
++  Commands output at the end of the file
++---------------------------------------------------
+
+begin FOOTER
+
++"[N] G00 [ZH]"
+"[N] G00 [SAFEZ]"
+"[N] M05"
+"[N] M30"
+%
